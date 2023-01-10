6b1278e5cd4b8a78ac3ca25e93ea824ded870dd4
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-10 22:45:34 +0000

    more commit

---
+++ src/StreamIntegerManipulation/ArraysAndStreams.java
@@ -0,0 +1,10 @@
+package StreamIntegerManipulation;
+
+import java.util.Arrays;
+
+public class ArraysAndStreams {
+    public static void main(String[] args) {
+        Integer[] values = {1,2,3,4,5,6,7,8,9,10};
+        System.out.printf("Original values: %s%n", Arrays.asList(values));
+    }
+}
