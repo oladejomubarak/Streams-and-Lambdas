5abbc5876e1c136632adaf2b6dfdb18ada297eac
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-10 23:09:58 +0000

    more commit

--- src/StreamIntegerManipulation/ArraysAndStreams.java
+++ src/StreamIntegerManipulation/ArraysAndStreams.java
@@ -1,10 +1,15 @@
 package StreamIntegerManipulation;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class ArraysAndStreams {
     public static void main(String[] args) {
-        Integer[] values = {1,2,3,4,5,6,7,8,9,10};
+        Integer[] values = {1,0,4,2,5,6,8,6,9,10};
         System.out.printf("Original values: %s%n", Arrays.asList(values));
+
+        System.out.printf("Sorted values: %s%n ",
+                Arrays.stream(values).sorted().collect(Collectors.toList())
+                );
     }
 }
