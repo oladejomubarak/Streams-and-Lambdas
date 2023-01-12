a3833b7c609f3b8553f1cb01d6253d18d7506aff
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-12 12:51:31 +0000

    more commit

---
+++ src/IncreaseDecrease.java
@@ -0,0 +1,14 @@
+public class IncreaseDecrease {
+
+
+       public static void main(String[] args) {
+            int count = 0;
+
+           int switchCount = 0;
+        switch (switchCount){
+
+
+        }
+
+    }
+}
