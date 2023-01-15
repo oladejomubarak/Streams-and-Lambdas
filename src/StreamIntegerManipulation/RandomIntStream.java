65989075aa648bedbaa1f3537ca912fcc5ba4337
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-15 14:54:14 +0000

    more commit

--- src/StreamIntegerManipulation/RandomIntStream.java
+++ src/StreamIntegerManipulation/RandomIntStream.java
@@ -1,7 +1,16 @@
 package StreamIntegerManipulation;
 
+import java.security.SecureRandom;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class RandomIntStream {
     public static void main(String[] args) {
-
+        SecureRandom random = new SecureRandom();
+        System.out.printf("%-6s%s%n", "Face", "Frequency");
+        random.ints(60_000_000, 1, 7)
+                .boxed()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .forEach((face, frequency)-> System.out.printf("%-6d%d%n", face, frequency));
     }
 }
