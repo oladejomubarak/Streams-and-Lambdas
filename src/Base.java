2fc669078b2951a4c679d8f1bcac74318e6e02ba
oladejomubarak <oladejomubarakade@gmail.com>
2023-01-27 03:57:14 +0000

    more commit

---
+++ src/Base.java
@@ -0,0 +1,31 @@
+public class Base {
+
+
+        void method(int a)
+        {
+            System.out.println("Base class method called with integer a = "+a);
+        }
+
+        void method(double d)
+        {
+            System.out.println("Base class method called with double d ="+d);
+        }
+    }
+
+    class Derived extends Base
+    {
+        @Override
+        void method(double d)
+        {
+            System.out.println("Derived class method called with double d ="+d);
+        }
+    }
+
+    class Main
+    {
+        public static void main(String[] args)
+        {
+            new Derived().method(10.0);
+        }
+    }
+
--- src/Lambdas.java
+++
@@ -1,5 +0,0 @@
-public class Lambdas {
-    public static void main(String[] args) {
-
-    }
-}
