dacde1f41e63851a7c8a07ac5c1ebd269d51381b
oladejomubarak <oladejomubarakade@gmail.com>
2023-02-09 12:55:43 +0000

    creating content

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+        static int a =50;
+        public static void main(String[] args) {
+            int a =10;
+            a=Test.a=90;
+            System.out.println(a);
+            System.out.println(Test.a);
+        }
+}
